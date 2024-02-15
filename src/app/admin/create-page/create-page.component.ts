04d6d0ed21517475481255dc676d1c9da216bd19
AlexLeoChumak <chumak1106@gmail.com>
2024-02-15 16:56:37 +0300

    create form for new post

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IPost } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-create-page',
   templateUrl: './create-page.component.html',
-  styleUrls: ['./create-page.component.scss']
+  styleUrls: ['./create-page.component.scss'],
 })
 export class CreatePageComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required),
+    });
   }
 
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const post: IPost = {
+      ...this.form.value,
+      date: new Date(),
+    };
+
+    // const { title, text, author } = this.form.value;
+    // const post: IPost = {
+    //   text,
+    //   title,
+    //   author,
+    //   date: new Date(),
+    // };
+  }
 }
