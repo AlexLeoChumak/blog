b6c72fdf6c86d3e9cfbb2fb889143414ebc9ee28
AlexLeoChumak <chumak1106@gmail.com>
2024-02-20 15:15:33 +0300

    create home-page, post-page, post

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPost } from '../shared/interfaces';
+import { PostsService } from '../shared/posts.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  posts$!: Observable<IPost[]>;
 
-  constructor() { }
+  constructor(private postService: PostsService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.posts$ = this.postService.getAll();
   }
-
 }
